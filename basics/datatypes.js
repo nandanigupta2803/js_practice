51a9673eae1a035b6e8bb5d8a99e2644ba27c0d2
nandanigupta2803 <130914272+nandanigupta2803@users.noreply.github.com>
2026-04-26 22:43:45 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,17 @@
+/*
+number 
+bigint
+string
+boolean - true/false
+null - standalone value
+undefined - not assined
+symbol - unique
+
+object
+*/
+console.log(typeof("Nandani"))
+console.log(typeof("Nandani "))
+let age = 28
+console.log(typeof(age))
+console.log(typeof(null))
+console.log(typeof(undefined))
