f82aa5ed39a0463b1f65d8007383c10144e8bc03
nandanigupta2803 <130914272+nandanigupta2803@users.noreply.github.com>
2026-06-07 11:48:32 +0000

    while and do while loop

---
+++ Iterations/two.js
@@ -0,0 +1,22 @@
+// While loop
+let i = 0
+// while (i <= 10) {
+//     console.log(`vlaue of i is ${i}`);
+//     i++
+// }
+// let myArray = [0, 5, 10, 15, 20]
+// let arr = 0
+
+
+// while (arr < myArray.length) {
+//     console.log(myArray[arr]);
+//     arr++
+// }
+
+
+// do while loop
+score = 0
+do {
+    console.log(`Value is ${score}`);
+    score++
+} while (score < 10);
