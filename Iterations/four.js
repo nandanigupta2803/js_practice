1c858280d202d090c5b97f54c40cfe2e1481881d
nandanigupta2803 <130914272+nandanigupta2803@users.noreply.github.com>
2026-06-11 19:49:07 +0000

    For in loop

---
+++ Iterations/four.js
@@ -0,0 +1,28 @@
+const myObj = {
+    js : 'Javascript',
+    cpp : 'C++',
+    rb : 'Ruby',
+    swift : 'swift by apple'
+}
+//for printing keys
+for (const key in myObj) { 
+    //console.log(key);
+}
+// for printing values
+for (const key in myObj) {
+    //console.log(myObj[key]);
+}
+//for key value pair
+for (const key in myObj) {
+    //console.log(`${key} is shortcut for ${myObj[key]}`);
+}
+ // For in loop in array
+const language = ["js", "react","cpp"]
+for (const key in language) {
+    //console.log(key);
+    // here we are getting keys of the array that is technically index so for values
+    console.log(language[key]);
+}
+/* This is the main difference b/w for of and for in loop 
+In for of we can get the values directly but in for in loop we get keys
+*/
