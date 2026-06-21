638b400758f1f2c8761f8772b149e5fe60face4e
nandanigupta2803 <130914272+nandanigupta2803@users.noreply.github.com>
2026-06-21 11:07:37 +0000

    theory about async

---
+++ events/Async.js
@@ -0,0 +1,15 @@
+// in this theory about async functions
+/*
+Javascript -> Synchronous (sequential),
+single threaded
+this is default behaviour
+
+Execution Context - execute one line of code at a time
+Each operation waits for the last one to complete before executing
+
+Blocking code - Block the flow of program - Read file Sync
+Non - blocking code - doest not block execution - read file Async
+
+In fetch() there is use of promise that is high priority queue 
+it is used when we want to know whether the task was successful or not
+*/
